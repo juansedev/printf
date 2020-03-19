01e83b56e95cf68830224f952390f0ac48822938
Juan Sebastian <juanllano93@gmail.com>
2020-03-19 03:01:44 +0000

    add function print int

--- formats.c
+++ formats.c
@@ -34,3 +34,68 @@ int print_string(va_list list)
 	}
 	return (char_add);
 }
+
+/**
+ * set_mult - funcion that return acount of digit of string in 10 base
+ * @lenght: actual leght to print
+ * Return: Will return leght of string stored.
+ */
+int set_mult(int lenght)
+{
+	int i = 1;
+
+	while (lenght)
+	{
+		i *= 10;
+		lenght--;
+	}
+	return (i);
+}
+
+/**
+ * print_int - Prints strings
+ * @list: list of arguments
+ * Return: Will return leght of string stored.
+ */
+int print_int(va_list list)
+{
+	int number = va_arg(list, int);
+	int lenght = 0, i, stop = 0, copy = 0, unit = 0;
+	int thounsand = 0, muliple = 0, sing = 1;
+
+	copy = number;
+
+	if (copy < 0)
+	{
+		putchar('-');
+		sing = -1;
+	}
+
+	for (i = 0; stop != 1; i++)
+	{
+		copy = copy / 10;
+		if ((copy * sing) < 10)
+			stop = 1;
+	}
+
+	lenght = i;
+	thounsand = lenght;
+
+	for (i = 0; i < thounsand; i++)
+	{
+		muliple = set_mult(lenght);
+		unit = number / muliple;
+		number = number - (muliple * unit);
+		printf("%d", unit * sing);
+		if (i == (thounsand - 1))
+		{
+			unit = number % 10;
+			printf("%d", unit * sing);
+		}
+		lenght--;
+	}
+
+	return (thounsand + 1)
+
+}
+
